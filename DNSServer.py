b7fa15e6de9d2d92475e7528dda9ef54fef01369
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-19 01:05:47 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -58,7 +58,7 @@ def generate_sha256_hash(input_string):
 
 # A dictionary containing DNS records mapping hostnames to different types of DNS data.
 dns_records = {
-    'example.com':{
+    'example.com.':{
         dns.rdatatype.A: '192.168.1.101',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
         dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
