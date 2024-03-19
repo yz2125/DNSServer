a054477e91e45f7651d4d6a1671f3b0f7071218f
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-19 01:07:25 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -88,7 +88,7 @@ dns_records = {
         dns.rdatatype.A: '192.168.1.105',
     },
     'nyu.edu.': {
-        #dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.A: '192.168.1.106',
         dns.rdatatype.TXT: [encrypted_value],
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
