3a9ebbbb8266a6b7a5af03f88d2f730a92b1ad5e
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-18 22:18:41 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -43,9 +43,9 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = b'abcdefghabcdefgh' # Remember it should be a byte-object
-password = 'YourPassword'
-input_string = 'YourSecretData'
+salt = b'somesalt' # Remember it should be a byte-object
+password = 'somepassword'
+input_string = 'somedata'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
