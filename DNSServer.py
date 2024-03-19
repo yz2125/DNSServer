f1e8bae87f83f0583a679c972409239f97b628d8
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-18 22:41:47 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -44,8 +44,8 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = b'somesalt' # Remember it should be a byte-object
-password = b'somepassword'
-input_string = b'somedata'
+password = 'somepassword'
+input_string = 'somedata'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
