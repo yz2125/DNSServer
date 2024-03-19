569b4d4794830e793479d4cdfba0dee2be0f1137
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-19 00:32:11 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -60,7 +60,7 @@ def generate_sha256_hash(input_string):
 dns_records = {
     'example.com':{
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: encrypted_value,  # Replace 'encrypted_secret_data' with your encrypted secret data
+        dns.rdatatype.TXT: key,  # Replace 'encrypted_secret_data' with your encrypted secret data
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
