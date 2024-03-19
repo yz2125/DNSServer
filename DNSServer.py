b6a626bf2fde7857f4921eb0c119652a3c5ba8c9
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-19 00:28:08 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -64,7 +64,7 @@ dns_records = {
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        #dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.TXT: ('This is a TXT record',),
         dns.rdatatype.SOA: (
             'ns1.example.com.', #mname
             'admin.example.com.', #rname
